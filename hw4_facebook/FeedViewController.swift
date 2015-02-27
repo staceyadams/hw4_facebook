2ca51bdd35125c779eea8f6de8f0235fba9b34f3
Stacey Adams <sleung@zynga.com>
2015-02-26 20:31:50 -0800

    tapping photos go to the photo view

--- hw4_facebook/FeedViewController.swift
+++ hw4_facebook/FeedViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UIViewControllerTransitioningDelegate, UIViewControllerAnimatedTransitioning {
+    
+    var isPresenting: Bool = true
+  //  var interactiveTransition: UIPercentDrivenInteractiveTransition!
+    
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var newsFeedImage: UIImageView!
     
@@ -25,15 +29,74 @@ class FeedViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func animationControllerForPresentedController(presented: UIViewController!, presentingController presenting: UIViewController!, sourceController source: UIViewController!) -> UIViewControllerAnimatedTransitioning! {
+        isPresenting = true
+        return self
+    }
+    
+    func animationControllerForDismissedController(dismissed: UIViewController!) -> UIViewControllerAnimatedTransitioning! {
+        isPresenting = false
+        return self
+    }
+    
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
+        // The value here should be the duration of the animations scheduled in the animationTransition method
+        return 0.4
+    }
+    
+//    func interactionControllerForPresentation(animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+//        interactiveTransition = UIPercentDrivenInteractiveTransition()
+//        //Setting the completion speed gets rid of a weird bounce effect bug when transitions complete
+//        interactiveTransition.completionSpeed = 0.99
+//        return interactiveTransition
+//    }
+    
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        println("animating transition")
+        var containerView = transitionContext.containerView()
+        var toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
+        var fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
+        
+        if (isPresenting) {
+            containerView.addSubview(toViewController.view)
+            toViewController.view.alpha = 0
+            UIView.animateWithDuration(0.4, animations: { () -> Void in
+                toViewController.view.alpha = 1
+                }) { (finished: Bool) -> Void in
+                    transitionContext.completeTransition(true)
+            }
+                    println("test1")
+        } else {
+            UIView.animateWithDuration(0.4, animations: { () -> Void in
+                fromViewController.view.alpha = 0
+                }) { (finished: Bool) -> Void in
+                    transitionContext.completeTransition(true)
+                    fromViewController.view.removeFromSuperview()
+                    println("test2")
+            }
+        }
+    }
+
+
 
     /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
     */
 
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        var destinationVC = segue.destinationViewController as PhotoViewController
+        destinationVC.modalPresentationStyle = UIModalPresentationStyle.Custom
+        destinationVC.transitioningDelegate = self
+        
+    }
+   
+    
+    @IBAction func didTapPhoto(sender: UITapGestureRecognizer) {
+        performSegueWithIdentifier("photoSegue", sender: self)
+    }
+    
+    
+    
 }
---
+++ hw4_facebook/PhotoViewController.swift
@@ -0,0 +1,42 @@
+//
+//  PhotoViewController.swift
+//  hw4_facebook
+//
+//  Created by Stacey Adams on 2/26/15.
+//  Copyright (c) 2015 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
+    @IBAction func didPressDone(sender: UIButton)
+    {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+}
